371a1c7ca78d959bc38001d95896bbda5fe29de8
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-03 18:18:59 +0000

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+use clap_verbosity_flag::Verbosity;
+
+#[derive(Debug, Parser)]
+struct Arguments {
+    #[clap(subcommand)]
+    command: Command,
+
+    #[clap(flatten)]
+    verbosity: Verbosity,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    #[clap(name = "build")]
+    Build {
+        #[clap(short, long, help = "Print generated PHP code to stdout.")]
+        stdout: bool,  
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::parse();
+
+    env_logger::Builder::new()
+        .filter_level(arguments.verbosity.log_level_filter())
+        .init();
+
+    match arguments.command {
+        Command::Build { stdout } => {
+            log::trace!("Starting build command...");
+            log::info!("Build command set to print to stdout: {}", stdout);
+        },
+    };
 }
