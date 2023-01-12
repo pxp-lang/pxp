c4c689044a4b589abf44639edaa877c3a569a717
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-12 19:26:51 +0000

    cmd/init: add basic init command

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,5 +1,23 @@
+use std::{path::PathBuf, fs::write};
+use colored::*;
+
 use super::InitCommand;
 
 pub fn run(args: InitCommand) {
-    dbg!(args);
+    let stub = include_str!("../../stubs/pxp.toml");
+    let path = PathBuf::from("./pxp.toml");
+
+    if path.exists() && ! args.force {
+        eprintln!("{} {}", "Error:".red().bold(), "configuration file already exists");
+        return;
+    }
+
+    match write(path, stub) {
+        Ok(_) => {
+            println!("{} {}", "Success:".green().bold(), "configuration file created");
+        },
+        Err(error) => {
+            eprintln!("{} {}", "Error:".red().bold(), error.to_string().red());
+        },
+    }
 }
