6db0eacd136421cf19ce782bf6caa093a23ef36c
Ryan Chandler <ryangjchandler@gmail.com>
2023-05-19 00:48:23 +0100

    wip

---
+++ app/Commands/AnalyseCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Commands;
+
+use App\Common\Configuration\Configuration;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class AnalyseCommand extends Command
+{
+    protected $signature = 'analyse';
+
+    protected $description = 'Statically analyse your PXP and PHP code, together.';
+
+    public function handle(Configuration $configuration)
+    {
+        $this->warn('Static analysis is still a work-in-progress and might not produce accurate results!');
+    }
+}
