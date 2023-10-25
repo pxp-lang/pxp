d2eee25154801cf509722a4d36b3a3572b10f44f
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-25 17:22:40 +0100

    Token: Store the literal ByteString for a Token

--- crates/pxp-token/src/token.rs
+++ crates/pxp-token/src/token.rs
@@ -1,3 +1,4 @@
+use pxp_bytestring::ByteString;
 use pxp_span::{Span, HasSpan};
 
 use crate::TokenKind;
@@ -5,15 +6,20 @@ use crate::TokenKind;
 pub struct Token {
     pub kind: TokenKind,
     pub span: Span,
+    pub literal: ByteString,
 }
 
 impl Token {
-    pub fn new(kind: TokenKind, span: Span) -> Self {
-        Self { kind, span }
+    pub fn new(kind: TokenKind, span: Span, literal: ByteString) -> Self {
+        Self { kind, span, literal }
     }
 
     pub fn kind(&self) -> &TokenKind {
         &self.kind
+    }
+
+    pub fn literal(&self) -> &ByteString {
+        &self.literal
     }
 }
 
