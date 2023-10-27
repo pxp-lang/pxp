ce6999b218f6a2b98e7157032734f80e21cffe4d
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-27 22:13:26 +0100

    Type: Add in all valid PHP types

--- crates/pxp-type/src/lib.rs
+++ crates/pxp-type/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use pxp_bytestring::ByteString;
+use pxp_span::Span;
+
+#[derive(Debug, Clone)]
+pub struct Type {
+    pub kind: TypeKind,
+    pub span: Span,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone)]
+pub enum TypeKind {
+    Named(ByteString),
+    Nullable(Box<Type>),
+    Union(Box<Type>),
+    Intersection(Box<Type>),
+    Void,
+    Null,
+    True,
+    False,
+    String,
+    Bool,
+    Int,
+    Float,
+    Array,
+    Object,
+    Mixed,
+    Callable,
+    Iterable,
+    StaticReference,
+    SelfReference,
+    ParentReference,
+    /// This is a special type that indicates the underlying type has been resolved,
+    /// resulting in a fully qualified name for union/intersection/named types.
+    Resolved(Box<Type>),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl TypeKind {
+    pub fn is_resolved(&self) -> bool {
+        match self {
+            TypeKind::Resolved(_) => true,
+            _ => false,
+        }
     }
 }
