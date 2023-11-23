0d4933ed2b5f8b0fdd47edf2dba58a26589b90a5
Ryan Chandler <ryangjchandler@gmail.com>
2023-11-23 20:02:40 +0000

    Diagnostics: Support expecting multiple token kinds

--- crates/pxp-diagnostics/src/kind.rs
+++ crates/pxp-diagnostics/src/kind.rs
@@ -1,12 +1,11 @@
 use std::fmt::Display;
 
-use pxp_span::Position;
 use pxp_token::{Token, TokenKind};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum DiagnosticKind {
     UnexpectedToken { token: Token },
-    ExpectedToken { expected: TokenKind, found: Token },
+    ExpectedToken { expected: Vec<TokenKind>, found: Token },
     UnexpectedEndOfFile,
 }
 
@@ -14,7 +13,11 @@ impl Display for DiagnosticKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             DiagnosticKind::UnexpectedToken { token } => write!(f, "unexpected token: {:?}", token),
-            DiagnosticKind::ExpectedToken { expected, found } => write!(f, "unexpected token {:?}, expected {:?}", found, expected),
+            DiagnosticKind::ExpectedToken { expected, found } => if expected.len() == 1 {
+                write!(f, "unexpected token {:?}, expected {:?}", found, expected)
+            } else {
+                write!(f, "unexpected token {:?}, expected one of {:?}", found, expected.iter().map(|kind| format!("{:?}", kind)).collect::<Vec<_>>().join(", "))
+            },
             DiagnosticKind::UnexpectedEndOfFile => write!(f, "unexpected end of file"),
         }
     }
