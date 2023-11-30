c7b96bcb435ea694d0cb5d6f810dc6023e5f1daa
Ryan Chandler <ryangjchandler@gmail.com>
2023-11-30 21:12:05 +0000

    Docblock: Remove existing code and start rethinking things

--- crates/pxp-docblock/src/ast/text.rs
+++
@@ -1,6 +0,0 @@
-use pxp_symbol::Symbol;
-
-#[derive(Debug, Clone)]
-pub struct TextNode {
-    pub text: Symbol,
-}
--- crates/pxp-docblock/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod lexer;
-mod ast;
-
-pub use lexer::{Lexer, token::{Token, TokenKind}};
