cfb05b412f8a1e63187d38b96975cd761ba44286
Ryan Chandler <ryangjchandler@gmail.com>
2024-02-06 13:31:01 +0100

    Visitor: Start implementing new NameResolvingVisitor and NameResolver

--- crates/pxp-visitor/src/name_resolver.rs
+++ crates/pxp-visitor/src/name_resolver.rs
@@ -1,16 +1,51 @@
+use std::collections::HashMap;
+
+use pxp_ast::{identifiers::SimpleIdentifier, NodeId, Statement};
+use pxp_symbol::Symbol;
+
 use crate::Visitor;
 
 #[derive(Debug, Default)]
 pub struct NameResolvingVisitor {
-    // ...
+    resolver: NameResolver,
 }
 
 impl NameResolvingVisitor {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn resolve(&mut self, ast: &mut [Statement]) -> NameResolver {
+        self.visit(ast);
+        self.resolver.clone()
+    }
 }
 
 impl Visitor for NameResolvingVisitor {
-    
+    fn visit_simple_identifier(&mut self, node: &mut SimpleIdentifier) {
+        
+    }
+}
+
+/// Internal structure for tracking AST state.
+struct State {
+    namespace: Option<Symbol>,
+    uses: HashMap<Symbol, Symbol>,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct NameResolver {
+    map: HashMap<NodeId, Symbol>,
+}
+
+impl NameResolver {
+    pub(crate) fn insert(&mut self, id: NodeId, symbol: Symbol) {
+        self.map.insert(id, symbol);
+    }
+
+    // Not using an `Option<Symbol>` here is a deliberate choice.
+    // We should only ever be requesting resolved symbols for nodes that we know can be resolved.
+    pub fn resolve(&self, id: NodeId) -> Symbol {
+        self.map[&id]
+    }
 }
