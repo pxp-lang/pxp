938d7aba119caa466280f22ff3511ca666c42d90
Ryan Chandler <ryangjchandler@gmail.com>
2024-02-09 17:48:20 +0000

    Parser: Remove some unused macros

--- crates/pxp-parser/src/macros.rs
+++ crates/pxp-parser/src/macros.rs
@@ -1,29 +1,4 @@
 #[macro_export]
-macro_rules! peek_token {
-    ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? => $out:expr),+ $(,)? ], $state:expr, [ $($message:literal),+ $(,)? ]) => {{
-        match &$state.stream.current().kind {
-            $(
-                $( $pattern )|+ $( if $guard )? => $out,
-            )+
-            _ => {
-                return $crate::expected_token_err!([ $($message,)+ ], $state);
-            }
-        }
-    }};
-    ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?),+ $(,)? ], $state:expr, [ $($message:literal),+ $(,)? ]) => {{
-        if !matches!($state.stream.current().kind, $( $pattern )|+ $( if $guard )?) {
-            return $crate::expected_token_err!([ $($message,)+ ], $state);
-        }
-    }};
-    ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? => $out:expr),+ $(,)? ], $state:expr, $message:literal) => {
-        $crate::peek_token!([ $($( $pattern )|+ $( if $guard )? => $out,)+ ], $state, [$message])
-    };
-    ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?),+ $(,)? ], $state:expr, $message:literal) => {
-        $crate::peek_token!([ $($( $pattern )|+ $( if $guard )?,)+ ], $state, [$message])
-    };
-}
-
-#[macro_export]
 macro_rules! expect_token {
     ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? => $out:expr),+ $(,)? ], $state:expr, [ $($message:literal),+ $(,)? ]) => {{
         let token = $state.stream.current();
@@ -45,17 +20,6 @@ macro_rules! expect_token {
 }
 
 #[macro_export]
-macro_rules! expected_token_err {
-    ([ $($expected:literal),+ $(,)? ], $state:expr $(,)?) => {{
-        $crate::expected_token!([$($expected),+], $state)
-    }};
-
-    ($expected:literal, $state:expr $(,)?) => {
-        $crate::expected_token_err!([$expected], $state)
-    };
-}
-
-#[macro_export]
 macro_rules! expected_token {
     ([ $($expected:literal),+ $(,)? ], $state:expr $(,)?) => {{
         todo!("tolerant mode {:?}", $state.stream.current().span)
@@ -64,20 +28,6 @@ macro_rules! expected_token {
     ($expected:literal, $state:expr $(,)?) => {
         $crate::expected_token!([$expected], $state)
     };
-}
-
-#[macro_export]
-macro_rules! expected_scope {
-    ([ $($(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? => $out:expr),+ $(,)? ], $state:expr) => {{
-        match $state.scope().cloned()? {
-            $(
-                $( $pattern )|+ $( if $guard )? => $out,
-            )+
-            _ => {
-                return Err($crate::error::reached_unpredictable_state($state.stream.current().span));
-            }
-        }
-    }};
 }
 
 #[macro_export]
