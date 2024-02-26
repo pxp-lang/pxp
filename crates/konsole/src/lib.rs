f25e6d857311994035b44f8d3eac7f231794facf
Ryan Chandler <ryangjchandler@gmail.com>
2024-02-26 02:05:13 +0000

    Konsole: Ditch idea for now

--- crates/konsole/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod prelude;
-pub mod application;
-pub mod command;
-pub mod output;
-pub mod input;
--- crates/konsole/src/prelude.rs
+++
@@ -1,4 +0,0 @@
-pub use crate::application::Application;
-pub use crate::command::Command;
-pub use crate::output::Output;
-pub use crate::input::Input;
