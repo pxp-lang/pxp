0a884259d1e480d0e74216e04236147b71c90a52
Ryan Chandler <ryangjchandler@gmail.com>
2024-03-03 02:33:44 +0000

    StdIndexes: Start writing build script to generate indexes

--- crates/pxp-std-indexes/build.rs
+++ crates/pxp-std-indexes/build.rs
@@ -1,5 +1,34 @@
+use std::{path::PathBuf, process::Command};
+
 const VERSIONS: [&str; 4] = ["8.0", "8.1", "8.2", "8.3"];
 
 fn main() {
+    clean_stubs_directory();
+    generate_stubs();
+
+    for version in VERSIONS {
+        generate_index_for_version(version);
+    }
+}
+
+fn clean_stubs_directory() {
+    let stubs_directory = PathBuf::from(format!("{}/stubs", env!("CARGO_MANIFEST_DIR")));
+    
+    if !stubs_directory.exists() {
+        return;
+    }
+
+    std::fs::remove_dir_all(&stubs_directory).unwrap();
+}
+
+fn generate_stubs() {
+    Command::new("php")
+        .arg("./separate-stubs-into-versions.php")
+        .current_dir(format!("{}/meta", env!("CARGO_MANIFEST_DIR")))
+        .spawn()
+        .expect("Failed to generate stubs.");
+}
+
+fn generate_index_for_version(version: &str) {
     
 }
