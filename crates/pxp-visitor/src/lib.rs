568e852eeaf1b8dfc20db9362d97bb49f20bd1c7
Ryan Chandler <ryangjchandler@gmail.com>
2024-05-23 21:54:54 +0100

    Visitor: Generate basic walk method skeletons

--- crates/pxp-visitor/src/lib.rs
+++ crates/pxp-visitor/src/lib.rs
@@ -1,5 +1,9 @@
 mod visitor;
+mod visitor_mut;
 mod walk;
+mod walk_mut;
 
-pub use visitor::{VisitorMut, Visitor};
+pub use visitor::Visitor;
+pub use visitor_mut::VisitorMut;
 pub use walk::*;
+pub use walk_mut::*;
