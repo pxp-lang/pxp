4223d5e38e9651b34136f39e7d4438f769762532
Ryan Chandler <ryangjchandler@gmail.com>
2024-06-04 18:22:28 +0100

    Parser/Diagnostics: Better formatting of diagnostics

--- crates/pxp-parser/src/diagnostics.rs
+++ crates/pxp-parser/src/diagnostics.rs
@@ -58,7 +58,7 @@ pub enum ParserDiagnostic {
 impl Display for ParserDiagnostic {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParserDiagnostic::UnexpectedToken { token } => write!(f, "unexpected token: {}", token.kind),
+            ParserDiagnostic::UnexpectedToken { token } => write!(f, "unexpected token {}", token.kind),
             ParserDiagnostic::ExpectedToken { expected, found } => {
                 if expected.len() == 1 {
                     write!(
