1ec54463700ce3f9c6e00cfb02a2933e322cefe8
Ryan Chandler <ryangjchandler@gmail.com>
2024-06-08 21:53:19 +0100

    Indexer: Track current namespace while indexing

--- crates/pxp-index/src/indexer.rs
+++ crates/pxp-index/src/indexer.rs
@@ -1,5 +1,6 @@
-use pxp_symbol::SymbolTable;
-use pxp_visitor::Visitor;
+use pxp_symbol::{Symbol, SymbolTable};
+use pxp_visitor::{walk_braced_namespace, walk_unbraced_namespace, Visitor};
+use pxp_ast::{UnbracedNamespace, *};
 
 use crate::Index;
 
@@ -7,8 +8,34 @@ use crate::Index;
 pub struct Indexer {
     index: Index,
     symbol_table: SymbolTable,
+    context: IndexerContext,
+}
+
+impl Indexer {
+    pub fn new(symbol_table: SymbolTable) -> Self {
+        Indexer {
+            index: Index::new(),
+            symbol_table,
+            context: IndexerContext::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct IndexerContext {
+    namespace: Option<Symbol>,
 }
 
 impl Visitor for Indexer {
-    
+    fn visit_unbraced_namespace(&mut self, node: &UnbracedNamespace) {
+        self.context.namespace = Some(node.name.as_resolved().unwrap().resolved);
+        walk_unbraced_namespace(self, node);
+        self.context.namespace = None;
+    }
+
+    fn visit_braced_namespace(&mut self, node: &BracedNamespace) {
+        self.context.namespace = node.name.as_ref().map(|n| n.as_resolved().unwrap().resolved);
+        walk_braced_namespace(self, node);
+        self.context.namespace = None;
+    }
 }
