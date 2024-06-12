349bab5f31ee6f1ef0a614cfba944d6366767426
Ryan Chandler <ryangjchandler@gmail.com>
2024-06-12 20:11:23 +0100

    Visitor: Regenerate visitors

--- crates/pxp-visitor/src/lib.rs
+++ crates/pxp-visitor/src/lib.rs
@@ -1,12 +1,12 @@
+mod node_visitor;
 #[allow(unused)]
 mod visitor;
 mod visitor_mut;
 mod walk;
 mod walk_mut;
-mod node_visitor;
 
+pub use node_visitor::NodeVisitor;
 pub use visitor::Visitor;
 pub use visitor_mut::VisitorMut;
 pub use walk::*;
 pub use walk_mut::*;
-pub use node_visitor::NodeVisitor;
