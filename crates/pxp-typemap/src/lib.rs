ed836463ca2da01092372cdc8648dc6cde993871
Ryan Chandler <ryangjchandler@gmail.com>
2024-07-31 17:32:01 +0100

    TypeMap: Add TypeMap struct

--- crates/pxp-typemap/src/lib.rs
+++ crates/pxp-typemap/src/lib.rs
@@ -1,10 +1,24 @@
-use pxp_ast::Statement;
+use std::collections::HashMap;
+
+use pxp_ast::{NodeId, Statement};
 use pxp_index::Index;
+use pxp_type::Type;
 
 pub struct TypeMapGenerator;
 
 impl TypeMapGenerator {
     pub fn generate(index: &Index, ast: &[Statement]) {
 
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct TypeMap {
+    map: HashMap<NodeId, Type>,
+}
+
+impl TypeMap {
+    pub fn new() -> Self {
+        Self::default()
     }
 }
